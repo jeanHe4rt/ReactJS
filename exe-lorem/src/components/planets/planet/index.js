aab9e1ce73a3133c45cf11413a0759a3a26d0a43
Jean Maidana <jean0king@gmail.com>
2021-11-11 22:59:06 -0300

    Adicionando Props nos App

--- exe-lorem/src/components/planets/planet/index.js
+++ exe-lorem/src/components/planets/planet/index.js
@@ -1,25 +1,13 @@
 import React from "react";
-import './index.css'
+import GreyImg from "../../../shared/grey-img";
 
-const Planet = () => {
+
+const Planet = (props) => {
   return (
     <div>
-      <h4>Mercurio</h4>
-      <p>Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema
-        Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem
-        a maior excentricidade e o seu eixo apresenta a menor inclinação em
-        relação ao plano da órbita dentre todos os planetas do Sistema Solar.
-        Mercúrio completa três rotações em torno de seu eixo a cada duas 
-        órbitas. O periélio da órbita de Mercúrio apresenta uma precessão de
-        5 600 segundos de arco por século, um fenômeno completamente explicado
-        apenas a partir do século XX pela Teoria da Relatividade Geral formulada
-        por Albert Einstein.[2] A sua aparência é brilhante quando observado da
-        Terra, tendo uma magnitude aparente que varia de −2,6 a 5,7, embora não
-        seja facilmente observado pois sua separação angular do Sol é de apenas
-        28,3º. Uma vez que Mercúrio normalmente se perde no intenso brilho solar,
-        exceto em eclipses solares, só pode ser observado a olho nu durante o
-        crepúsculo matutino ou vespertino.</p>
-      <img class="grey-img" src="./Mercurio.png" alt="Planeta Mercurio"/>
+      <h4>{props.name}</h4>
+      <p>{props.description}</p>
+      <GreyImg img_url={props.img_url}/>
     </div>
   )
 }
