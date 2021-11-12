1ed51356c1a2a480aec308fcfc2fc684eacc144d
Jean Maidana <jean0king@gmail.com>
2021-11-11 21:22:18 -0300

    feat: :art: My-FIrst-Component

---
+++ my-first-app/src/Hello.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+// class HelloWorld extends React.Component {
+//   render () {
+//     return <h1> Hello World</h1>
+//   }
+// }
+
+// function HelloWorld() {
+//   return <h1> Hello World</h1>
+// }
+
+const HelloWorld = () => {
+  return <h1> Hello World</h1>
+}
+
+export default HelloWorld;
