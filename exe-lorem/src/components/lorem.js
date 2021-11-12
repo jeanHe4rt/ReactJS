1190ac805e956d330a9c219d8450ce054cdbb5fa
Jean Maidana <jean0king@gmail.com>
2021-11-11 22:03:45 -0300

    Resolucao Exe

---
+++ exe-lorem/src/components/lorem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoremIpsum = () => {
+  return <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia sapiente
+    et, iure molestias vero, maiores sed ullam velit itaque est odit ea eum ad,
+    mollitia ipsa voluptates totam voluptas voluptatem.
+  </p>
+}
+
+export default LoremIpsum;
